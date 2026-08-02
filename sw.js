7ec8b1758be2c35dc8d8dafe67a45b276c206529
karenofthecat <lives.suitably@gmail.com>
2026-08-02 15:13:14 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='iwato-kakeibo-v064';
+const CACHE='iwato-kakeibo-v065';
 const ASSETS=['./','./index.html','./manifest.json','./icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
